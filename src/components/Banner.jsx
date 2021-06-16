f52c5d6ac37c79785e8c1c1704ca06430f1ec549
yangbreoll <78190786+yangbreoll@users.noreply.github.com>
2021-06-16 12:48:45 +0900

    modify banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,6 +3,7 @@ import {
   BannerSection,
   BannerSlide,
   Button,
+  ButtonWrap,
   DotButtons,
 } from "../styles/screen/BannerStyle";
 import "./banner.css";
@@ -10,45 +11,36 @@ export default function Banner() {
   const TOTAL_SLIDES = 5;
   const imgUrl = "/img/";
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [currentBtn, setCurrentBtn] = useState(0);
   const slideRef = useRef(null);
   const dotRef = useRef(null);
+
   const nextSlide = () => {
     if (currentSlide >= TOTAL_SLIDES) {
       setCurrentSlide(0);
+      setCurrentBtn(0);
+      dotRef.current.childNodes[TOTAL_SLIDES].classList.remove("white");
     } else {
       setCurrentSlide(currentSlide + 1);
-    }
-    const currentSlider = document.querySelector(".white");
-    if (currentSlider) {
-      currentSlider.classList.remove("white");
-      const nextSlide = currentSlider.nextElementSibling;
-      if (nextSlide) {
-        nextSlide.classList.add("white");
-      } else {
-        dotRef.current.childNodes[0].classList.add("white");
-      }
+      setCurrentBtn(currentSlide + 1);
+      dotRef.current.childNodes[currentBtn].classList.remove("white");
     }
   };
   const prevSlide = () => {
     if (currentSlide === 0) {
       setCurrentSlide(TOTAL_SLIDES);
+      setCurrentBtn(TOTAL_SLIDES);
+      dotRef.current.childNodes[0].classList.remove("white");
     } else {
       setCurrentSlide(currentSlide - 1);
-    }
-    const currentSlider = document.querySelector(".white");
-    if (currentSlider) {
-      currentSlider.classList.remove("white");
-      const preSlide = currentSlider.previousElementSibling;
-      if (preSlide) {
-        preSlide.classList.add("white");
-      } else {
-        dotRef.current.childNodes[TOTAL_SLIDES].classList.add("white");
-      }
+      setCurrentBtn(currentSlide - 1);
+      dotRef.current.childNodes[currentBtn].classList.remove("white");
     }
   };
   const matchBtn = (e) => {
     const id = e.target.id;
     setCurrentSlide(+id);
+    setCurrentBtn(+id);
     slideRef.current.style.transition = "transform 0.4s ease-in-out";
     slideRef.current.style.transform = `translateX( -${id}00%)`;
     const currentSlide = document.querySelector(".white");
@@ -60,6 +52,7 @@ export default function Banner() {
   useEffect(() => {
     slideRef.current.style.transition = "all 0.3s ease-in-out";
     slideRef.current.style.transform = `translateX(-${currentSlide}00%)`;
+    dotRef.current.childNodes[currentBtn].classList.add("white");
     let id = setInterval(nextSlide, 3000);
     return () => clearInterval(id);
   });
@@ -73,14 +66,16 @@ export default function Banner() {
         <img src={imgUrl + "5.jpg"} alt={"banner"} />
         <img src={imgUrl + "6.jpg"} alt={"banner"} />
       </BannerSlide>
-      <Button left onClick={prevSlide}>
-        <i className="fas fa-chevron-left"></i>
-      </Button>
-      <Button onClick={nextSlide}>
-        <i className="fas fa-chevron-right"></i>
-      </Button>
+      <ButtonWrap>
+        <Button left onClick={prevSlide}>
+          <i className="fas fa-chevron-left"></i>
+        </Button>
+        <Button onClick={nextSlide}>
+          <i className="fas fa-chevron-right"></i>
+        </Button>
+      </ButtonWrap>
       <DotButtons ref={dotRef}>
-        <button onClick={matchBtn} className="white" id="0"></button>
+        <button onClick={matchBtn} id="0"></button>
         <button onClick={matchBtn} id="1"></button>
         <button onClick={matchBtn} id="2"></button>
         <button onClick={matchBtn} id="3"></button>
--- src/styles/screen/BannerStyle.js
+++ src/styles/screen/BannerStyle.js
@@ -1,29 +1,40 @@
 import styled from "styled-components";
+import { flexCenter, maxWidth } from "../variables";
 
 export const BannerSection = styled.section`
   overflow: hidden;
   position: relative;
+  ${flexCenter}
 `;
 export const BannerSlide = styled.div`
   display: flex;
   width: 100%;
-
+  max-width: 1920px;
+  margin: 0 auto;
   img {
     width: 100%;
     height: 370px;
     object-fit: cover;
   }
 `;
-export const Button = styled.button`
+export const ButtonWrap = styled.div`
   position: absolute;
   top: 47.5%;
+  ${maxWidth}
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Button = styled.button`
   left: ${(props) => (props.left ? "18%" : "")};
   right: ${(props) => (props.left ? "" : "18%")};
   background-color: transparent;
   border: none;
   font-size: 25px;
-  max-width: 1280px;
   color: white;
+  margin: 0 20px;
+  text-shadow: 0px 1px 3px rgba(0, 0, 0, 0.5);
+  cursor: pointer;
 `;
 export const DotButtons = styled.div`
   position: absolute;
@@ -36,5 +47,7 @@ export const DotButtons = styled.div`
     padding: 5px;
     border-radius: 5555px;
     border: none;
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.5);
+    cursor: pointer;
   }
 `;
